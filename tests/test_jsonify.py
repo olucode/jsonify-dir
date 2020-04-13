d8f797287bd1d6da1932dae296c45215e7d87da6
Olumide Falomo <olumide@kudi.co>
2020-04-13 11:14:00 +0100

    Add tests

---
+++ tests/test_jsonify.py
@@ -0,0 +1,17 @@
+import unittest
+import src.jsonify as jsonify
+
+
+class JsonifyTestCase(unittest.TestCase):
+    def test_load_dir(self):
+        structure = jsonify.load_dir('./src')
+
+        self.assertTrue('jsonify.py' in structure,
+                        'jsonify.py absent in "src/" directory')
+        self.assertEqual(structure['jsonify.py'], None,
+                         'jsonify.py is not a file')
+
+    def test_empty_structure_for_fake_path(self):
+        structure = jsonify.load_dir('./src/hello')
+
+        self.assertEqual(structure, {})
